7b2337ca0a95f9bf8ad3e4ee7ce6047e5ec754ab
Corey Ford <coyotebush22@gmail.com>
2012-09-05 11:32:52 -0700

    add password setting and messages

--- password.php
+++ password.php
@@ -1,3 +1,23 @@
+<?php
+define('PASSWD_FILE', '../htpasswd');
+$user = $_SERVER['REMOTE_USER'];
+
+if (isset($_POST['password'])) {
+	$password = $_POST['password'];
+	if (isset($_POST['password_confirm']) && $password == $_POST['password_confirm']) {
+		$command = 'htpasswd -b ' . escapeshellarg(PASSWD_FILE)
+			. ' ' . escapeshellarg($user) . ' ' . escapeshellarg($password);
+		exec($command, $output, $return);
+
+		if ($return != 0)
+			$message = array('error', 'Error setting password: ' . $return);
+		else
+			$message = array('success', 'Password changed. You will be asked to log in again.');
+	}
+	else $message = array('error', 'Passwords do not match');
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,15 +30,22 @@ h1 { margin: 0 0 0.5em; text-align: center; }
 table { margin: 0 auto; }
 td, th { padding: 0.2em 0.5em; }
 th { text-align: right; }
+p.message { margin: 0.5em 0; padding: 0.3em 0; text-align: center; }
+p.message.error { background: #f77; }
+p.message.success { background: #ff4; }
 </style>
 </head>
 <body>
 <div id="main">
 <h1>Change password</h1>
+<?php if (isset($message)): ?>
+<p class="message <?php echo $message[0] ?>"><?php echo htmlspecialchars($message[1]) ?></p>
+<?php endif ?>
+<form action="" method="post">
 <table>
 	<tr>
 		<th>Username</th>
-		<td><?php echo htmlspecialchars($_SERVER['REMOTE_USER']); ?></td>
+		<td><?php echo htmlspecialchars($user); ?></td>
 	</tr>
 	<tr>
 		<th>New password</th>
@@ -33,6 +60,7 @@ th { text-align: right; }
 		<td><input type="submit" value="Change password"></td>
 	</tr>
 </table>
+</form>
 </div>
 </body>
 </html>
