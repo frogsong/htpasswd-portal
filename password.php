212242571b3135a83cd8f031507edc5ebc93236c
Corey Ford <coyotebush22@gmail.com>
2012-09-05 10:32:17 -0700

    add password fields, submit button

--- password.php
+++ password.php
@@ -1,7 +1,19 @@
 <h1>Change password</h1>
 <table>
 	<tr>
-		<td>Logged in as</td>
+		<th>Username</th>
 		<td><?php echo $_SERVER['REMOTE_USER']; ?></td>
+	</tr>
+	<tr>
+		<th>New password</th>
+		<td><input type="password" name="password" size="20" maxlength="40"></td>
+	</tr>
+	<tr>
+		<th>New password (again)</th>
+		<td><input type="password" name="password_confirm" size="20" maxlength="40"></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td><input type="submit" value="Change password"></td>
 	</tr>
 </table>
